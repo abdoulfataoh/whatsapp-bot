55a2e2dd2c60be917c178fee73aa83d675e5f11f
abdoulfataoh <abdoulfataoh@gmail.com>
2022-09-20 14:19:38 +0000

    update parameters

--- app/settings.py
+++ app/settings.py
@@ -5,6 +5,6 @@ from environs import Env
 
 env = Env()
 
-
-API_URL = env.url('API_URL', 'https://graph.facebook.com/v14.0')
-PHONE_NUMBER_ID = env.str('PHONE_NUMBER_ID', '102703642606293')
+API_URL = env.str('API_URL', 'https://graph.facebook.com/v14.0')
+PHONE_NUMBER_ID = env.str('PHONE_NUMBER_ID', '107797488755439')
+TOKEN = env.str('TOKEN', 'EAAHIGqFaZAoUBAB0EyPKAKebIpgfvypWeKK1XsGLU5UGJRRZChNgZAgtV0lHYYSnbEEm6UepwqjnWUfSKcgGsENFqhJYFzSCyKHYUQu3bm220GH4sekZBSNgFL2AB00iDHfwIYzVraIgQtvNZAQHvqLJGjwFAWlsWJYBZBeG9z0BK7OMcMmrnBrp1ZCP7paOgS4gNpwpjMSxQZDZD')
